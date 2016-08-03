3d40c4efb01811076a97811d593aa5ab83b8183e
TanGuo <TanGuo@6003adc8-6d71-4505-8932-1eed1e498793>
2016-08-03 12:15:08 +0000

    商品API

---
+++ src/main/java/com/tcz/api/controller/ItemController.java
@@ -0,0 +1,36 @@
+package com.tcz.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Controller - 商品
+ * 
+ * @author candy.tam
+ *
+ */
+@RestController
+@RequestMapping("/item")
+public class ItemController {
+	
+	// 正在揭晓
+	public void raffleItems(){
+		
+	}
+
+	// 热门推荐
+	public void hotItems(){
+		
+	}
+	
+	// 即将揭晓
+	public void soonItems(){
+		
+	}
+	
+	// 新品上架
+	public void newItems(){
+		
+	}
+	
+}
