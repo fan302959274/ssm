1017b5dac46d4f6dff711afdeceb46c958e8f131
TanGuo <TanGuo@6003adc8-6d71-4505-8932-1eed1e498793>
2016-08-04 12:54:13 +0000

    添加切面

--- src/main/java/com/tcz/api/config/aop/WebLogAspect.java
+++ src/main/java/com/tcz/api/config/aop/WebLogAspect.java
@@ -1,6 +1,7 @@
 package com.tcz.api.config.aop;
 
 import java.util.Arrays;
+import java.util.Date;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -20,12 +21,14 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 public class WebLogAspect {
 
     private Logger logger = LoggerFactory.getLogger(getClass());
+    private long time = 0;
 
     @Pointcut("execution(public * com.tcz.api.controller.*.*(..))")
     public void webLog(){}
 
     @Before("webLog()")
     public void doBefore(JoinPoint joinPoint) throws Throwable {
+    	time = new Date().getTime();
         // 接收到请求，记录请求内容
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         HttpServletRequest request = attributes.getRequest();
@@ -36,13 +39,14 @@ public class WebLogAspect {
         logger.info("IP : " + request.getRemoteAddr());
         logger.info("CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
         logger.info("ARGS : " + Arrays.toString(joinPoint.getArgs()));
-
+        
     }
 
     @AfterReturning(returning = "ret", pointcut = "webLog()")
     public void doAfterReturning(Object ret) throws Throwable {
         // 处理完请求，返回内容
         logger.info("RESPONSE : " + ret);
+        logger.info("end call time : {} " , new Date().getTime() - time);
     }
 
 }
