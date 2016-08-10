0862246149bf05a167134144935a974057612549
TanGuo <TanGuo@6003adc8-6d71-4505-8932-1eed1e498793>
2016-08-10 11:20:19 +0000

    首页

--- trunk/tcz-api/src/main/java/com/tcz/api/service/impl/AdServiceImpl.java
+++ trunk/tcz-api/src/main/java/com/tcz/api/service/impl/AdServiceImpl.java
@@ -1,14 +1,10 @@
 package com.tcz.api.service.impl;
 
-import java.util.ArrayList;
 import java.util.List;
-
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.tcz.api.mapper.AdMapper;
-import com.tcz.api.model.po.Ad;
 import com.tcz.api.model.vo.item.AdVo;
 import com.tcz.api.service.AdService;
 
@@ -19,14 +15,7 @@ public class AdServiceImpl implements AdService {
 
 	@Override
 	public List<AdVo> findAds(Long adPositionId) {
-		List<Ad> ads = adMapper.findAds(adPositionId);
-		List<AdVo> adVos = new ArrayList<>();
-		for (int i = 0; i < ads.size(); i++) {
-			AdVo adVo = new AdVo();
-			BeanUtils.copyProperties(ads.get(i), adVo);
-			adVos.add(adVo);
-		}
-		return adVos;
+		return adMapper.findAds(adPositionId);
 	}
 	
 }
