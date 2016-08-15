c0556da9fa23bb716edbb869fa2a72811f9fef77
TanGuo <TanGuo@6003adc8-6d71-4505-8932-1eed1e498793>
2016-08-15 12:16:54 +0000

    首页

--- src/main/java/com/tcz/api/controller/IndexController.java
+++ src/main/java/com/tcz/api/controller/IndexController.java
@@ -5,13 +5,10 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.tcz.api.model.po.Lottery;
 import com.tcz.api.model.vo.item.AdVo;
 import com.tcz.api.model.vo.item.CurrentShoppVo;
 import com.tcz.api.model.vo.item.LotteryVo;
