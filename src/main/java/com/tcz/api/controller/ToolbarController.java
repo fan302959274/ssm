9ddbb31c70f0f58adc96f8c0af7aed42dbd579d5
TanGuo <TanGuo@6003adc8-6d71-4505-8932-1eed1e498793>
2016-08-16 13:25:09 +0000

    工具条

---
+++ src/main/java/com/tcz/api/controller/ToolbarController.java
@@ -0,0 +1,30 @@
+package com.tcz.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tcz.api.model.po.Member;
+import com.tcz.api.service.MemberService;
+import com.tcz.core.rest.Message;
+
+/**
+ * Controller - toolbar
+ * 
+ * @author candy.tam
+ *
+ */
+@RestController
+@RequestMapping("/toolbar")
+public class ToolbarController {
+	@Autowired
+	MemberService memberService;
+
+	@RequestMapping("/currentMember")
+	public Message currentMember(){
+		Member member = new Member();
+		member.setUserPhoto("http://faceimg.1yyg.com/UserFace/20160614184701355.jpg");
+		member.setName("candy.tam");
+		return Message.success(member);
+	}
+}
