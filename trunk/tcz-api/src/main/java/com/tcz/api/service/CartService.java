f37edfc0fd47138d5479478864d589f31e782465
TanGuo <TanGuo@6003adc8-6d71-4505-8932-1eed1e498793>
2016-08-25 07:36:55 +0000

    优化购物车

---
+++ trunk/tcz-api/src/main/java/com/tcz/api/service/CartService.java
@@ -0,0 +1,23 @@
+package com.tcz.api.service;
+
+import java.util.List;
+import java.util.Map;
+
+public interface CartService {
+	
+	/**
+	 * 获取当前购物车
+	 * 
+	 * @return
+	 */
+	Map<String, Object> getCurrent(String cartDataSel);
+	
+	/**
+	 * 获取商品购物车信息
+	 * 
+	 * @param id
+	 * @return
+	 */
+	List<Map<String, Object>> getCartItemInfo(Long id);
+	
+}
